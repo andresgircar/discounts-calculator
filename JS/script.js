7138271104aca6073c29346feba28e4d6b9f457a
Andres Giraldo <andresgircar@gmail.com>
2022-04-10 20:03:50 -0500

    actualizando estilos y añadiendo primeras funciones

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+function calcularPrecioConDescuento (precio, descuento) {
+    const porcentajeDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajeDescuento)/100;
+    return precioConDescuento;
+}
+function calcularPrecioFinal() {
+    const inputPrice = document.getElementById("price-input");
+    const priceValue= inputPrice.value;
+    const inputDiscount = document.getElementById("discount-input");
+    const discountValue= inputDiscount.value;
+    const precioFinal = calcularPrecioConDescuento(priceValue, discountValue);
+    const resultP = document.getElementById("output-text");
+    resultP.innerText = "El precio que debes pagar después de aplicar el descuento es: $" + precioFinal;
+}
